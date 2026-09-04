72ec7d80ea227a5442ea9d4f15710eac91a155e0
SIDDHANT8766 <gadkari.siddhant.vivek@gmail.com>
2026-09-04 10:07:59 +0530

    StudentController.java

--- src/main/java/com/divandsection/siddhantspringboot/controller/StudentController.java
+++ src/main/java/com/divandsection/siddhantspringboot/controller/StudentController.java
@@ -2,6 +2,7 @@ package com.divandsection.siddhantspringboot.controller;
 
 import com.divandsection.siddhantspringboot.entity.Student;
 import com.divandsection.siddhantspringboot.repository.StudentRepository;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,32 +16,53 @@ public class StudentController {
     StudentRepository srobj;
 
 
+    // ADD STUDENT
+
     @PostMapping("/add")
-    public Student addStudent(@RequestBody Student student)
-    {
+    public Student addStudent(@RequestBody Student student) {
+
         return srobj.save(student);
     }
 
+
+    // GET ALL STUDENTS
+
     @GetMapping("/all")
     public List<Student> getStudents() {
+
         return srobj.findAll();
     }
 
+
+    // GET STUDENT BY ID
+
     @GetMapping("/{id}")
     public Student getStudent(@PathVariable Long id) {
+
         return srobj.findById(id).get();
     }
 
+
+    // UPDATE STUDENT
+
     @PutMapping("/{id}")
-    public Student updateStudent(@PathVariable Long id,
-                                 @RequestBody Student student) {
+    public Student updateStudent(
+            @PathVariable Long id,
+            @RequestBody Student student) {
 
         Student s = srobj.findById(id).get();
 
         s.setName(student.getName());
+        s.setEmail(student.getEmail());
+        s.setAddress(student.getAddress());
+        s.setPhone(student.getPhone());
+        s.setAge(student.getAge());
 
         return srobj.save(s);
     }
+
+
+    // DELETE STUDENT
 
     @DeleteMapping("/{id}")
     public String deleteStudent(@PathVariable Long id) {
