4916cee5e847848ce6e8877ee6ccbd09c9bd4510
SIDDHANT8766 <gadkari.siddhant.vivek@gmail.com>
2026-09-04 10:06:01 +0530

    Student.java

--- src/main/java/com/divandsection/siddhantspringboot/entity/Student.java
+++ src/main/java/com/divandsection/siddhantspringboot/entity/Student.java
@@ -1,57 +1,81 @@
 package com.divandsection.siddhantspringboot.entity;
 
-
 import jakarta.persistence.*;
 
-
 @Entity
-@Table(name="student")
+@Table(name = "student")
 public class Student {
-
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-
     private Long id;
+
     private String name;
+    private String email;
+    private String address;
+    private String phone;
+    private Integer age;
 
-
-    public Student()
-    {
+    public Student() {
 
     }
 
+    public Student(Long id, String name, String email,
+                   String address, String phone, Integer age) {
 
-    public Student(Long id, String name)
-    {
-        this.id=id;
-        this.name=name;
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.address = address;
+        this.phone = phone;
+        this.age = age;
     }
 
-
-
-    public Long getId()
-    {
+    public Long getId() {
         return id;
     }
 
-
-    public void setId(Long id)
-    {
-        this.id=id;
+    public void setId(Long id) {
+        this.id = id;
     }
 
-
-
-    public String getName()
-    {
+    public String getName() {
         return name;
     }
 
-
-    public void setName(String name)
-    {
-        this.name=name;
+    public void setName(String name) {
+        this.name = name;
     }
 
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
 }
